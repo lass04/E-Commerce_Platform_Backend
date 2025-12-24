eb37fc02e08ac2c359d227af67c0a703815b16e3
lass04 <yassinelassoued58@gmail.com>
2025-12-24 18:47:44 +0100

    Added Authentication Middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,57 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = async (req) => {
+    
+    const auth = req.headers.authorization;
+    if(!auth || !auth.startsWith("Bearer"))
+        return null
+
+    const token = auth.split(" ")[1];
+
+    try{
+        return jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+    }catch{
+        return null
+    }
+}
+
+const adminOnly = async (req,res,next) => {
+    const user = verifyToken(req);
+
+    if(!user)
+        return res.status(403).json({
+            success:false,
+            message:"Invalid Token"
+        });
+
+
+    if(user.role!=="admin")
+        return res.status(403).json({
+            success:false,
+            message:"Admin priviliges needed !"
+        });
+ 
+        req.user = user;
+        next();
+    
+}
+
+const authenticate = async (req,res,next) => {
+
+    const user = verifyToken(req);
+
+    if(!user)
+        return res.status(403).json({
+            success:false,
+            message:"Invalid Token"
+        });
+
+    req.user = user;
+    next();
+
+}
+
+export { 
+    adminOnly,
+    authenticate
+}

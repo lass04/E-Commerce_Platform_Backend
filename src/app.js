35489e0a4f933260c10c64a94730257a6ea5244b
lass04 <yassinelassoued58@gmail.com>
2025-12-26 16:23:59 +0100

    feat: Cors Configuration

--- src/app.js
+++ src/app.js
@@ -6,12 +6,21 @@ import categoryRouter from "./routes/category.route.js";
 import cartRouter from "./routes/cart.route.js";
 import productRouter from "./routes/product.route.js";
 import rateLimiter from "../middlewares/rateLimit.js";
+import cors from "cors";
 
 const app = express();
 
 // Request & Cookie Parsing Middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+// Cors Configuration
+app.use(cors({
+    origin:"http://localhost:4002",
+    methods:["GET","POST","PUT","DELETE"],
+    allowHeaders: ["Content-Type","Authorization"],
+    credentials:true
+}));
 
 // Applying RateLimiter in all app routes
 app.use(rateLimiter);
